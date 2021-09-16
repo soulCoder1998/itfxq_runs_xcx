37a41276a79f3bfafc12505e4e4db57e0b8711c6
soulCoder1998 <65808686+soulCoder1998@users.noreply.github.com>
2021-09-16 16:59:42 +0800

    1111

---
+++ App.vue
@@ -0,0 +1,34 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			let username = uni.getStorageSync('loginUserName')
+			
+			if(!username || username==''){
+				
+				uni.navigateTo({
+					url:'/pages/login/login'
+				})
+			}
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		onError: function(){
+			console.log('应用出错')
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "uview-ui/index.scss";
+	/*每个页面公共css */
+	.page{
+		width:100%;
+		height: 100%;
+		background-image: url(static/bg.jpg);
+	}
+	
+</style>
---
+++ main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+// 引入全局uView
+import uView from 'uview-ui'
+Vue.use(uView);
+
+// 公共函数库
+import Main from './common/common.js'
+import api from './common/api.js'
+Vue.prototype.fun = Main;
+Vue.prototype.$api=api;
+const app = new Vue({
+    ...App
+})
+app.$mount()
